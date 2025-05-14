c2e37772c2cd6f475fbd447537b4acaa59eb362e
ongeziwe17 <ongeziwejunior6@gmail.com>
2025-05-15 00:21:23 +0200

    Project DB Migrations, Con Strings and **.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
+using TaskApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+// Configure SQL Server
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -12,6 +18,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate(); // this will apply migrations and create the DB if it doesn't exist
+}
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
